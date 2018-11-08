496bb8ce5495b8c9cf90173145f03431356b29f7
Jesse Hallam <jesse.hallam@gmail.com>
2018-11-08 01:39:34 -0500

    MM-12908: defer enter sending until 500ms after channel switch
    It is unlikely that you want to send a draft within 500ms of switching channels. We could refine this by detecting additional typed characters as intent otherwise, but this keeps it simple.
    
    I didn't touch creating a comment, since that never gets focussed on a channel switch.

--- components/create_post/create_post.jsx
+++ components/create_post/create_post.jsx
@@ -230,6 +230,7 @@ export default class CreatePost extends React.Component {
         };
 
         this.lastBlurAt = 0;
+        this.lastChannelSwitchAt = 0;
         this.draftsForChannel = {};
     }
 
@@ -267,6 +268,7 @@ export default class CreatePost extends React.Component {
 
     componentDidUpdate(prevProps) {
         if (prevProps.currentChannel.id !== this.props.currentChannel.id) {
+            this.lastChannelSwitchAt = Date.now();
             this.focusTextbox();
         }
     }
@@ -494,7 +496,7 @@ export default class CreatePost extends React.Component {
     postMsgKeyPress = (e) => {
         const {ctrlSend, codeBlockOnCtrlEnter, currentChannel} = this.props;
 
-        const {allowSending, withClosedCodeBlock, message} = postMessageOnKeyPress(e, this.state.message, ctrlSend, codeBlockOnCtrlEnter);
+        const {allowSending, withClosedCodeBlock, message} = postMessageOnKeyPress(e, this.state.message, ctrlSend, codeBlockOnCtrlEnter, Date.now(), this.lastChannelSwitchAt);
 
         if (allowSending) {
             e.persist();
